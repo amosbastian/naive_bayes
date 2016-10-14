9f834e529fa904d866e39574d5a312303f1a12a9
Amos Bastian <amos_bastian@hotmail.com>
2016-10-14 20:01:54 +0200

    Works a bit better, just testing still

--- naive_bayes.py
+++ naive_bayes.py
@@ -9,11 +9,6 @@ def extract_vocabulary(D):
     
     return Counter(nltk.word_tokenize(all_terms))
 
-def docs_in_class(D, c):
-    print "Getting amount of documents in class {}...".format(c)
-    D_filtered = D[D.ministerie == c]
-    return len(D_filtered.titel)
-
 def text_in_class(D, c):
     print "Getting text from documents in class {}...".format(c)
     D_filtered = D[D.ministerie == c]
@@ -21,34 +16,35 @@ def text_in_class(D, c):
     return nltk.word_tokenize(all_terms) 
 
 def count_tokens(text_c, t):
-    print t
     return text_c.count(t)
 
 def train_multinomial(C, D):
-    condition_probability = defaultdict(Counter)
+    condprob = defaultdict(lambda: defaultdict(float))
+    prior = defaultdict(float)
     V = extract_vocabulary(D)
-    N = len(D.titel)
     for c in C:
-        print c
-        N_c = docs_in_class(D, c)
-        prior_c = float(N_c) / N
+        print "\n   " + c
+        prior[c] = C[c]
         text_c = text_in_class(D, c)
-        for t, _ in V.most_common(20):
+        for t, _ in V.most_common(3):
             T_ct = count_tokens(text_c, t)
-            condition_probability[t][c] = "hehe" # probability uitrekenen hier
+            condprob[t][c] = float((T_ct + 1)) / (len(text_c) + 20)
 
-    print condition_probability
+    print V, prior, condprob
 
 if __name__ == '__main__':
     # Change to KVR1000.csv.gz if this becomes too slow for you
-    df = pd.read_csv('KVR.csv', sep='\t', encoding='utf-8', index_col=0, 
+    D = pd.read_csv('KVR.csv', sep='\t', encoding='utf-8', index_col=0, 
         names=['jaar', 'partij','titel','vraag','antwoord','ministerie']) 
 
     # Get value counts and create a dictionary of the classes we want
-    class_counts = df.ministerie.value_counts(normalize=True).head(20)
-    classes = dict(zip(class_counts.index.tolist()[1::2], 
-        class_counts.tolist()[1::2])) #
+    cc = D.ministerie.value_counts(normalize=True).head(20)
+    C = dict(zip(cc.index.tolist()[1::2], cc.tolist()[1::2]))
 
     # Filter out rows with unwanted classes
-    df_filtered = df[df.ministerie.isin(classes.keys())]
-    train_multinomial(classes, df_filtered)
+    D_filtered = D[D.ministerie.isin(C.keys())]
+    cc_filtered = D_filtered.ministerie.value_counts(normalize=True).head(10)
+    C_filtered = dict(zip(cc_filtered.index.tolist(), 
+        D_filtered.ministerie.value_counts(normalize=True).head(10).tolist()))
+
+    train_multinomial(C_filtered, D_filtered)
